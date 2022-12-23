c5d32fdcab87556d0e928900030d113789c42aeb
cononi <ekgoqhffo@gmail.com>
2022-12-24 02:25:48 +0900

    [feat] useQuery IsLoading IseError error 추가

--- querystudy/src/study/Posts.tsx
+++ querystudy/src/study/Posts.tsx
@@ -17,20 +17,30 @@ const fetchPosts = async (): Promise<Post[]> => {
     return response.json();
 }
 
-const Posts = () => {
+function Posts() {
     const [currentPage, setCurrentPage] = useState(0);
     const [selectedPost, setSelectedPost] = useState<Post>();
 
     // 쿼리 키, 데이터를 가져오는 비동기 함수 선언(매핑하는 데이터는 fetchPosts에서 반환된 데이터가 되고 HTTP 요청에서 반환된 JSON이 된다.)
-    const { data } = useQuery<Post[], Error>(['posts'], fetchPosts)
+    // 구조 분해 방식
+    const { data, isError, error, isLoading } = useQuery<Post[], Error>(['posts'], fetchPosts)
 
     // array map은 배열전용 이기 떄문에 현재 데이터가 정의되지 않았다고 나온다. 
     // fetchPosts가 비동기이기 때문에 실행 컨텍스트 관점에서 보면 비동기가 실행되기전에 해당 컴포넌트의 return가 먼저 실행되기 때문에 해당 내용이 없다고 나온다.
-    if (!data) return <div></div>
+    // --구현체 
+    // if (!data) return <div></div> 
+
+    // 로드 중인지 확인
+    if (isLoading) return <h3>Loading...</h3>
+    // 에러로 받아오지 못할 때 조기 반환한다.
+    if (isError) return <>
+        <h3>Not Data</h3>
+        <p>{error.message}</p>
+    </>
 
     return (
-        <>
-            <table>
+        <div style={{ display: 'flex', flexDirection: 'column', margin: '100px' }}>
+            <table border={1}>
                 <thead>
                     <tr>
                         <th>번호</th>
@@ -39,7 +49,7 @@ const Posts = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map(post => {
+                    {data?.map(post => {
                         return (
                             <tr key={post.id}
                                 className="post-title"
@@ -53,10 +63,29 @@ const Posts = () => {
                     })}
                 </tbody>
             </table>
-            <div>
-                {selectedPost?.body}
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <button disabled onClick={() => { }}>
+                    Previous page
+                </button>
+                <span>Page {currentPage + 1}</span>
+                <button disabled onClick={() => { }}>
+                    Next page
+                </button>
             </div>
-        </>
+            <div>
+                <h3>나의 이야기</h3>
+                <button>삭제</button>
+                <button>타이틀 변경</button>
+                <div style={{
+                    border: '1px solid black',
+                }}>
+                    {selectedPost?.body}
+                </div>
+                <div>
+                    <h3>덧글</h3>
+                </div>
+            </div>
+        </div>
     )
 }
 
